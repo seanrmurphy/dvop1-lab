4b2dec1183d94a3bdac5c3a716c1a3543cd2e736
Sean Murphy <sean.murphy@buynow.tv>
2017-03-20 21:44:05 +0100

    Reverted to old test cases.

--- test/simple_python_service_test.py
+++ test/simple_python_service_test.py
@@ -1,40 +1,26 @@
 import datetime
 import unittest
 from simple_python_service import *
-import sys
-
-print sys.path
 
 class TestSimpleService(unittest.TestCase):
 
     def test_root_endpoint(self):
-	pass
+        return_val = root_endpoint()
+        self.assertEqual(return_val, 'CCP2 - Devops 1 Lab Demonstration Service!')
 
 
     def test_status(self):
-	pass
+        return_val = status()
+        self.assertEqual(return_val, '{"status": "OK"}')
 
 
     def test_hello(self):
-	pass
+        return_val = hello('Test')
+        now = datetime.datetime.now()
+        #{"welcome_string": "Hello Test - today is 19 Mar"}
+        comparison_str = '{\"welcome_string\": \"Hello Test - today is ' + datetime.datetime.strftime(now, "%d %b") + '\"}'  
+        self.assertEqual(return_val, comparison_str)
 
 
 if __name__ == '__main__':
     unittest.main()
-
-#     def test_upper(self):
-#         self.assertEqual('foo'.upper(), 'FOO')
-
-#     def test_isupper(self):
-#         self.assertTrue('FOO'.isupper())
-#         self.assertFalse('Foo'.isupper())
-
-#     def test_split(self):
-#         s = 'hello world'
-#         self.assertEqual(s.split(), ['hello', 'world'])
-#         # check that s.split fails when the separator is not a string
-#         with self.assertRaises(TypeError):
-#             s.split(2)
-
-# if __name__ == '__main__':
-#     unittest.main()
